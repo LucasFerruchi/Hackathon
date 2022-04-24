388e13c3a235e32ad8d68d16e4476ebb5597f113
LucasFerruchi <lucasferruchi05@gmail.com>
2022-04-24 13:53:42 -0300

    creacion de LandingPage, componente Slider y su css. Rutas publicas y privadas. Layout.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import PublicRoute from "./routes/PublicRoute";
+import LandingPage from "./pages/LandingPage";
 
 
 function App() {
   return (
-    <p>hola</p>
+    <> 
+    <Router>
+      <Layout>
+      <Routes>
+      <Route path="/" element={<PublicRoute><LandingPage /></PublicRoute>} />
+      </Routes>
+      </Layout>
+    </Router>
+    </>
   );
 }
 
