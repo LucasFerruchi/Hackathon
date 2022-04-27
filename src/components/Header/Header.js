fd2a4a90b75d28883cb147fb1d0f168f686a56fc
LucasFerruchi <lucasferruchi05@gmail.com>
2022-04-27 08:56:18 -0300

    cambios en estilos para volver la pagina RESPONSIVE

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,22 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="/">IN_Blockchain</Navbar.Brand>
-        <Nav className=" ml-5">
-          <Nav.Link href="#features">INformacion</Nav.Link>
-          <Nav.Link href="#features">INnovacion</Nav.Link>
-          <Nav.Link href="#features">INversion</Nav.Link>
-          <Nav.Link href="/homePage">INgresar</Nav.Link>
-        </Nav>
-    </Container>
-      </Navbar>
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Container className="d-flex align-items-center justify-content-between">
+          <Navbar.Brand href="/">IN_Blockchain</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav ">
+          <Nav className=" ml-5">
+            <Nav.Link href="#features">INformacion</Nav.Link>
+            <Nav.Link href="#features">INnovacion</Nav.Link>
+            <Nav.Link href="#features">INversion</Nav.Link>
+            <Nav.Link href="/homePage">INgresar</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    
   );
 };
 
